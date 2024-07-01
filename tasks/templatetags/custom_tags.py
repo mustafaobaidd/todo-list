bb082477f35d5f6a766fad7c90f94e93b7693e68
mustafaobaidd <dodor949@gmail.com>
2024-07-01 08:20:07 +0300

    add color bootstrap

--- tasks/templatetags/custom_tags.py
+++ tasks/templatetags/custom_tags.py
@@ -1,9 +1,12 @@
 # tasks/templatetags/custom_tags.py
 from django import template
+from django.utils.safestring import SafeString
 
 register = template.Library()
 
 
 @register.filter(name='add_class')
 def add_class(value, css_class):
+    if isinstance(value, SafeString):
+        return value
     return value.as_widget(attrs={'class': css_class})

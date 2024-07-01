2ecf7092f39aeb4100f3e9ea35810f8503796a3b
mustafaobaidd <dodor949@gmail.com>
2024-07-02 00:56:47 +0300

    update Settings

--- todo_list_project/settings.py
+++ todo_list_project/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/tasks/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "tasks/static",
 ]
 
 # Default primary key field type
